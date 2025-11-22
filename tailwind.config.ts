647a4dc7a4c6543e92c4ed87796f2a6f5e793a2d
Reza Khareisa <rezakhareisa@gmail.com>
2025-11-22 03:10:39 +0000

    Try fixing this error: `Build Error: Error evaluating Node.js code. Erro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,5 +102,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require("framer-motion/dist/framer-motion.cjs.js")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
